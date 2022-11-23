98af6f53ed566efd8d01481fb661888d5688af10
nurnabimiah <diunayonahmed@gmail.com>
2022-11-23 10:36:38 +0600

    Varibale related code

---
+++ Basic/print.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int m =10;
+    int s =16;
+    // new line print
+  cout << "Hello World!" <<endl;
+  cout << "Hello Nayon" <<endl;
+
+  // varible print
+  cout << n << " " <<m <<endl;
+
+
+  const float  pai = 3.1416;
+  cout << "Pi value is :" <<pai;
+  cout << s;
+
+
+
+  return 0;
+}
